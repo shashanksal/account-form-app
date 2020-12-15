b028418a4c2cbeaa1c5aeee8b01d3f324b4be8a0
Shashank Salunkhe <shashank.salunkhe@itinnovate.com.au>
2020-12-15 21:47:04 +1100

    Add material Ui, Add AppHeader component

--- src/components/Header/AppHeader.js
+++ src/components/Header/AppHeader.js
@@ -1,7 +1,69 @@
 import React from 'react';
 
+//Material UI imports
+import {
+	Grid,
+	AppBar,
+	Toolbar,
+	IconButton,
+	Typography,
+	Button,
+} from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+
+const useStyles = makeStyles((theme) =>
+	createStyles({
+		root: {
+			flexGrow: 1,
+		},
+		menuButton: {
+			marginRight: theme.spacing(2),
+		},
+		title: {
+			flexGrow: 1,
+			color: '#05AE95',
+			fontSize: 28,
+			fontWeight: 600,
+		},
+		button: {
+			textTransform: 'none',
+		},
+	})
+);
+
 const AppHeader = () => {
-	return <div>My Account</div>;
+	const classes = useStyles();
+	return (
+		<Grid container spacing={3}>
+			<Grid item md={4}></Grid>
+			<Grid item md={4}>
+				<AppBar position="static" color={'transparent'} elevation={0}>
+					<Toolbar>
+						<IconButton
+							edge="start"
+							className={classes.menuButton}
+							color="inherit"
+							aria-label="menu"
+						>
+							<ArrowBackIosIcon />
+						</IconButton>
+						<Typography
+							//variant="h4"
+							className={classes.title}
+							align="left"
+						>
+							My Account
+						</Typography>
+						<Button className={classes.button} color="inherit">
+							Save
+						</Button>
+					</Toolbar>
+				</AppBar>
+			</Grid>
+			<Grid item md={4}></Grid>
+		</Grid>
+	);
 };
 
 export default AppHeader;
